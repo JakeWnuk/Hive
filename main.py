60ebb7da523bcf24ab4fb1935b894060d39b8f1e
Jake Wnuk <jakewnuk@gmail.com>
2020-10-29 23:28:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import asyncio
 import concurrent.futures
 import os
+import time
 from ipaddress import ip_address
-from tqdm import tqdm
 
 
-def gen_ip_range(start, end):
+# needs argv
+# should this be in /16 rather than /24
+# is nmaptocsv the best way to agg results?
+
+
+async def gen_ip_range(start, end):
     """
     Generates IPv4 addresses inclusively
     :param start: start IPv4 address
     :param end:  end IPv4 address
     :return: list
     """
+    printer("Generating buzz for " + str(start) + " - " + str(end), event=True)
     start_int = int(ip_address(start).packed.hex(), 16)
     end_int = int(ip_address(end).packed.hex(), 16)
-    return [ip_address(ip).exploded for ip in tqdm(range(start_int, end_int), desc="Generating buzz for " + str(start))]
+    return [ip_address(ip).exploded for ip in range(start_int, end_int)]
 
 
 def chunk_list(lst, n):
@@ -26,37 +33,61 @@ def chunk_list(lst, n):
     return [lst[i:i + n] for i in range(0, len(lst), n)]
 
 
+def printer(msg, event=False, error=False, warn=False):
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = "\033[32m"
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
+    tm = time.strftime("%H:%M:%S")
+
+    if warn:
+        print(f'{bcolors.WARNING}{bcolors.BOLD}[ * ] [{tm}] {msg}{bcolors.ENDC}')
+    elif error:
+        print(f'{bcolors.FAIL}{bcolors.UNDERLINE}[ ! ] [{tm}] {msg}{bcolors.ENDC}')
+    elif event:
+        print(f'{bcolors.OKGREEN}[ + ] [{tm}] {msg}{bcolors.ENDC}')
+    else:
+        print(f'{bcolors.OKCYAN}[ - ] [{tm}] {msg}{bcolors.ENDC}')
+
+
 class Hive:
     """
     Hive Jobs: creates the bees, operates the bees for scanning and enum, and aggregates the bees reports
     """
 
-    def __init__(self):
+    def __init__(self, harvest=False, verbose=True):
         self.SubnetList = [
-            ("192.168.0.0", "192.168.255.255"), ("10.0.0.0", "10.255.255.255"), ("172.16.0.0", "172.16.255.255")]
+            ("192.168.0.0", "192.168.255.255")]  # , ("10.0.0.0", "10.255.255.255"), ("172.16.0.0", "172.16.255.255")]
         self.Bees = []
-        self._gen_bees()
+        asyncio.run(self._gen_bees(harvest, verbose))
 
-    def _gen_bees(self):
+    async def _gen_bees(self, harvest, verbose):
         """
         Generates all the bees for the hive
         """
 
-        for itr in self.SubnetList:
-            full_ip_range = gen_ip_range(itr[0], itr[1])
-            split_ip_range = chunk_list(full_ip_range, 256)
-
-            for i in split_ip_range:
-                self.Bees.append(Bee(i[0], i[-1]))
+        ip_ranges = await asyncio.gather(
+            *(gen_ip_range(itr[0], itr[1]) for itr in self.SubnetList)
+        )
+        for i in ip_ranges:
+            split_ip_ranges = chunk_list(i, 256)
+            for x in split_ip_ranges:
+                self.Bees.append(Bee(x[0], x[-1], harvest, verbose))
 
     def operate(self):
         """
         Tells bee class to scan all of the given ranges with multi-threading and found targets are harvested
         """
-        print("### DEPLOYING SWARM ###")
-
-        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-            list(tqdm(executor.map(Bee.is_alive, self.Bees), total=len(self.Bees), desc="Scanning"))
+        printer("! ! ! DEPLOYING SWARM ! ! !", warn=True)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            executor.map(Bee.is_alive, self.Bees)
 
         live_bees = []
         for i in self.Bees:
@@ -66,8 +97,9 @@ class Hive:
         self.Bees = live_bees
 
     def report(self):
-        print(len(self.Bees))
-        pass
+        printer("Hive Completed. Number of successful bees: " + str(len(self.Bees)), event=True)
+        for i in self.Bees:
+            printer(str(i._get_range()[0]) + "/24", event=True)
 
 
 class Bee:
@@ -75,23 +107,27 @@ class Bee:
     Bees are given an IP range to scan and then harvest IP's if their range has an alive IP address inside
     """
 
-    def __init__(self, ip_start, ip_end):
+    def __init__(self, ip_start, ip_end, harvest=False, verbose=True):
         self.name = "Bee-" + str(ip_start) + "-" + str(ip_end)
         self.ipRange = (ip_start, ip_end)
         self.live = False
+        self.harvest = harvest
+        self.verbose = verbose
 
     def is_alive(self):
         """
         Tells if the Bee's assigned IP range has alive IP's inside
         """
-        out = os.popen('fping -a -i 1 -r 1 -g ' + str(self.ipRange[0]) + '/24  2> /dev/null').read()
+        out = os.popen('fping -a -i 2 -r 4 -g ' + str(self.ipRange[0]) + '/24  2> /dev/null').read()
 
         if out == "":
-            pass
+            if self.verbose:
+                printer("No buzz in... " + str(self.ipRange[0]) + " to " + str(self.ipRange[1]))
         else:
-            tqdm.write("A bee found some flowers in... " + str(self.ipRange[0]) + " to " + str(self.ipRange[1]))
+            printer("A bee found some flowers in... " + str(self.ipRange[0]) + " to " + str(self.ipRange[1]), warn=True)
             self.live = True
-            self._harvest()
+            if self.harvest:
+                self._harvest()
 
     def get_status(self):
         """
@@ -108,11 +144,12 @@ class Bee:
         return self.ipRange
 
     def _harvest(self):
-        pass
-        # add more attacking code for sub
+        printer("Starting Nmap for " + self.name, event=True)
+        out = os.popen('nmap -n -Pn -p 80,443,22,21,23 ' + str(self.ipRange[0]) + "-255 2>/dev/null | nmaptocsv").read()
+        printer("Nmap finished for " + self.name, event=True)
 
 
 if __name__ == '__main__':
-    myHive = Hive()
+    myHive = Hive(harvest=False, verbose=True)
     myHive.operate()
     myHive.report()
